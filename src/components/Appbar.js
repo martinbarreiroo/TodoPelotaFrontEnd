1cfa6368431a5c41d725bdb4a508c967e5a7c2d7
martinbarreiroo <mbarreiro@mail.austral.edu.ar>
2024-03-22 01:15:57 -0300

    Add AppBar component and update App.js

---
+++ src/components/Appbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Appbar = () => {
+    return (
+        <div className="appbar">
+            <h1>TodoPelota</h1>
+            <button>Login</button>
+        </div>
+    );
+};
+
+export default Appbar;
