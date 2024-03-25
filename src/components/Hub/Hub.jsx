cded92c5fa920adca5f37848abfd6d0e2dcb9159
martinbarreiroo <mbarreiro@mail.austral.edu.ar>
2024-03-25 12:49:16 -0300

    Update Hub component styling and add icons to home menu buttons

--- src/components/Hub/Hub.jsx
+++ src/components/Hub/Hub.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import './Hub.css';
 import logo from '../assets/logo.png'
+import trofeo from '../assets/trofeo.png'
+import estadistica from '../assets/estadistica.png'
+import person from '../assets/person.png'
 
 export const Hub = () => {
     return (
         <div className="hub-container">
             <img src={logo} alt="Logo" className="logo" />
+                
                 <div className="home-menu">
-                    <button className="home-button">Mis Torneos</button>
-                    <button className="home-button">Mis Estadísticas</button>
+                    <button className="home-button">Mis Torneos
+                        <img src={trofeo} alt="trofeo" className="trofeo" />
+                    </button>
+                        
+                    <button className="home-button">Mis Estadísticas
+                        <img src={estadistica} alt="trofeo" className="estadistica" />
+                    </button>
+                        
                     <button className="home-button">Crear Torneo</button>
-                    <button className="home-button">Perfil</button>
+                    <button className="home-button">Perfil
+                        <img src={person} alt="trofeo" className="trofeo" />
+                    </button>
+                        
                 </div>
         </div>
     );
